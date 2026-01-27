7dc9ddd8888164bfde88bf0322011373314de0c3
Petr Tripolsky <tripolskypetr@gmail.com>
2026-01-27 16:55:52 +0500

    math

--- packages/front/src/lib/services/base/ExchangeService.ts
+++ packages/front/src/lib/services/base/ExchangeService.ts
@@ -5,6 +5,32 @@ import { CandleInterval, Exchange } from "backtest-kit";
 
 type ExchangeName = string;
 
+const INTERVAL_MINUTES: Record<CandleInterval, number> = {
+  "1m": 1,
+  "3m": 3,
+  "5m": 5,
+  "15m": 15,
+  "30m": 30,
+  "1h": 60,
+  "2h": 120,
+  "4h": 240,
+  "6h": 360,
+  "8h": 480,
+};
+
+const STEP_ADJUST_TICKS: Record<CandleInterval, number> = {
+  "1m": 960,
+  "3m": 320,
+  "5m": 192,
+  "15m": 64,
+  "30m": 32,
+  "1h": 16,
+  "2h": 8,
+  "4h": 4,
+  "6h": 2,
+  "8h": 2,
+};
+
 export class ExchangeService {
   private readonly loggerService = inject<LoggerService>(TYPES.loggerService);
 
@@ -17,9 +43,29 @@ export class ExchangeService {
     this.loggerService.log("exchangeService getCandles", {
       dto,
     });
-    return await Exchange.getRawCandles(dto.symbol, dto.interval, {
-      exchangeName: dto.exchangeName,
-    });
+
+    const step = INTERVAL_MINUTES[dto.interval];
+    const tick = STEP_ADJUST_TICKS[dto.interval];
+
+    if (!step || !tick) {
+      throw new Error(`Unsupported interval: ${dto.interval}`);
+    }
+
+    const offsetMs = tick * step * 60 * 1000;
+
+    const sDate = dto.currentTime - offsetMs;
+    const eDate = Math.min(dto.currentTime + offsetMs, Date.now() - 1);
+
+    return await Exchange.getRawCandles(
+      dto.symbol,
+      dto.interval,
+      {
+        exchangeName: dto.exchangeName,
+      },
+      undefined,
+      sDate,
+      eDate,
+    );
   };
 }
 
