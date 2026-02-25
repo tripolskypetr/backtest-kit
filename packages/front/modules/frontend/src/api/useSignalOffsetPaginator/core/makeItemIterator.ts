b3f8206600cd219e9bf2db2b6a4554a8efe08cba
Petr Tripolsky <tripolskypetr@gmail.com>
2026-02-25 18:09:32 +0500

    patch

--- packages/front/modules/frontend/src/api/useSignalOffsetPaginator/core/makeItemIterator.ts
+++ packages/front/modules/frontend/src/api/useSignalOffsetPaginator/core/makeItemIterator.ts
@@ -1,34 +1,35 @@
 import { IStorageSignalRow } from "backtest-kit";
+import { iteratePromise } from "react-declarative";
 import ioc from "../../../lib";
 import { ISignal } from "../model/Signal.model";
 
 type ClosedSignal = Extract<IStorageSignalRow, { status: "closed" }>;
 
-const makeItemIterator =
-  (mode: "live" | "backtest") => async (): Promise<ISignal[]> => {
-    const all =
-      mode === "live"
-        ? await ioc.storageViewService.listSignalLive()
-        : await ioc.storageViewService.listSignalBacktest();
+const makeItemIterator = (mode: "live" | "backtest") =>
+    iteratePromise(async (): Promise<ISignal[]> => {
+        const all =
+            mode === "live"
+                ? await ioc.storageViewService.listSignalLive()
+                : await ioc.storageViewService.listSignalBacktest();
 
-    return all
-      .filter((s): s is ClosedSignal => s.status === "closed")
-      .map(
-        (s): ISignal => ({
-          id: s.id,
-          symbol: s.symbol,
-          position: s.position,
-          takeProfitPrice: s.priceTakeProfit,
-          originalTakeProfitPrice: s.originalPriceTakeProfit,
-          stopLossPrice: s.priceStopLoss,
-          originalStopLossPrice: s.priceStopLoss,
-          profitLossPercentage: s.pnl.pnlPercentage,
-          buyPrice: s.pnl.priceOpen,
-          quantity: 0,
-          date: new Date(s.createdAt).toISOString(),
-          status: "finished",
-        })
-      );
-  };
+        return all
+            .filter((s): s is ClosedSignal => s.status === "closed")
+            .map(
+                (s): ISignal => ({
+                    id: s.id,
+                    symbol: s.symbol,
+                    position: s.position,
+                    takeProfitPrice: s.priceTakeProfit,
+                    originalTakeProfitPrice: s.originalPriceTakeProfit,
+                    stopLossPrice: s.priceStopLoss,
+                    originalStopLossPrice: s.priceStopLoss,
+                    profitLossPercentage: s.pnl.pnlPercentage,
+                    buyPrice: s.pnl.priceOpen,
+                    quantity: 0,
+                    date: new Date(s.createdAt).toISOString(),
+                    status: "finished",
+                }),
+            );
+    });
 
 export default makeItemIterator;
--- packages/front/modules/frontend/src/api/useSignalOffsetPaginator/useSignalOffsetPaginator.ts
+++ packages/front/modules/frontend/src/api/useSignalOffsetPaginator/useSignalOffsetPaginator.ts
@@ -1,14 +1,18 @@
-import { useOffsetPaginator } from "react-declarative";
+import { pickDocuments, useOffsetPaginator } from "react-declarative";
 import makeItemIterator from "./core/makeItemIterator";
 import { ISignal } from "./model/Signal.model";
 
 export const useSignalOffsetPaginator = (mode: "live" | "backtest") =>
-  useOffsetPaginator<ISignal>({
-    handler: async (limit, offset) => {
-      const fetchItems = makeItemIterator(mode);
-      const all = await fetchItems();
-      return all.slice(offset, offset + limit);
-    },
-  });
+    useOffsetPaginator<ISignal>({
+        handler: async (limit, offset) => {
+            const iter = pickDocuments<ISignal>(limit, offset);
+            for await (const document of makeItemIterator(mode)) {
+                if (iter([document]).done) {
+                    break;
+                }
+            }
+            return iter([]).rows;
+        },
+    });
 
 export default useSignalOffsetPaginator;
