cd3f7be094f69ffeb53f88e0c7658c5197b64e9b
Petr Tripolsky <tripolskypetr@gmail.com>
2026-03-03 22:11:55 +0500

    docs

--- src/classes/Broker.ts
+++ src/classes/Broker.ts
@@ -2,4 +2,6 @@
 // Broker.useAdapter
 // Broker.commitPartialProfit/Broker.commitAverageBuy on public api layer before DI query
 
+// Should listen syncSubject automatically cause the new signal is triggered by backtest-kit not the user
+
 export {}
