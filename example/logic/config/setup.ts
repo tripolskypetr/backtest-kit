3501d72ab7b41d4ba6b5c76543a04b430a49ef69
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-15 02:09:36 +0500

    patch

--- example/logic/config/setup.ts
+++ example/logic/config/setup.ts
@@ -3,9 +3,14 @@ import isToday from "dayjs/plugin/isToday";
 import localeData from "dayjs/plugin/localeData";
 import ruLocale from "dayjs/locale/ru";
 import utc from "dayjs/plugin/utc";
+import { setConfig } from "agent-swarm-kit";
 
 dayjs.extend(localeData);
 dayjs.extend(utc);
 dayjs.extend(isToday);
 
 dayjs.locale(ruLocale);
+
+setConfig({
+    CC_MAX_NESTED_EXECUTIONS: Infinity,
+})
