44fe45c4c674d8b79e9cdd924ac930f8d1fd86ed
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-17 20:17:22 +0500

    patch

--- example/logic/core/index.ts
+++ example/logic/core/index.ts
@@ -1,5 +1,6 @@
 import "./advisor/tavily_news.advisor";
-import "./advisor/stock_data.advisor";
+import "./advisor/stock_data_1m.advisor";
+import "./advisor/stock_data_15m.advisor";
 
 import "./completion/ollama_outline_tool.completion";
 import "./completion/ollama_outline_format.completion";
