fc5a6fac835e2e016b09918750286e22d6ddf13b
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-17 18:55:45 +0500

    wip

--- example/content/feb_2026.strategy/feb_2026.test.ts
+++ example/content/feb_2026.strategy/feb_2026.test.ts
@@ -5,7 +5,7 @@ import {
   Log,
 } from "backtest-kit";
 import { errorData, getErrorMessage } from "functools-kit";
-import { forecast } from "logic";
+import { forecast, reaction } from "logic";
 
 const forecastSource = Cache.file(
   async (symbol: string, when: Date, currentPrice: number) => {
@@ -16,10 +16,26 @@ const forecastSource = Cache.file(
   { interval: "4h", name: "forecast_source" },
 );
 
+const reactionSource = Cache.fn(
+  async (symbol: string, when: Date, currentPrice: number) => {
+    const forecast = await forecastSource(symbol, when, currentPrice);
+    const result = await reaction(forecast, symbol, when);
+    console.log(result, when);
+    return { ...result, currentPrice };
+  },
+  { interval: "4h" }
+);
+
 addStrategySchema({
   strategyName: "feb_2026_strategy",
   getSignal: async (symbol, when, currentPrice) => {
-    await forecastSource(symbol, when, currentPrice);
+    const { sentiment } = await forecastSource(symbol, when, currentPrice);
+    if (sentiment === "bullish") {
+      await reactionSource(symbol, when, currentPrice);
+    }
+    if (sentiment === "bearish") {
+      await reactionSource(symbol, when, currentPrice);
+    }
     return null;
   },
 });
