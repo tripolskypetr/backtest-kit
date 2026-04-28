1870ee47156314e929326b05c99c0f65b2e97047
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-28 17:15:44 +0500

    wip

---
+++ example/content/dec_2025.strategy/dec_2026.strategy.ts
@@ -0,0 +1,80 @@
+import {
+  addStrategySchema,
+  listenError,
+  listenActivePing,
+  Log,
+  Position,
+  commitClosePending,
+  getPositionPnlPercent,
+  getPositionEntryOverlap,
+  getPositionEntries,
+  commitAverageBuy,
+} from "backtest-kit";
+import { errorData, getErrorMessage, str } from "functools-kit";
+
+const HARD_STOP = 25.0;
+const TARGET_PROFIT = 3;
+
+const LADDER_STEP_COST = 100;
+const LADDER_UPPER_STEP = 5;
+const LADDER_LOWER_STEP = 1;
+
+const LADDER_MAX_STEPS = 10;
+
+addStrategySchema({
+  strategyName: "dec_2025_strategy",
+  getSignal: async (symbol, when, currentPrice) => {
+    return {
+      position: "long",
+      ...Position.moonbag({
+        position: "long",
+        currentPrice,
+        percentStopLoss: HARD_STOP,
+      }),
+      minuteEstimatedTime: Infinity,
+      cost: LADDER_STEP_COST,
+    };
+  },
+});
+
+listenActivePing(async ({ symbol, currentPrice }) => {
+  const { length: steps } = await getPositionEntries(symbol);
+  if (steps >= LADDER_MAX_STEPS) {
+    return;
+  }
+  const hasOverlap = await getPositionEntryOverlap(symbol, currentPrice, {
+    upperPercent: LADDER_UPPER_STEP,
+    lowerPercent: LADDER_LOWER_STEP,
+  });
+  if (hasOverlap) {
+    return;
+  }
+  await commitAverageBuy(symbol, LADDER_STEP_COST);
+});
+
+
+listenActivePing(async ({ symbol, data, timestamp }) => {
+  console.log(new Date(timestamp));
+  const currentProfit = await getPositionPnlPercent(symbol);
+  if (currentProfit < TARGET_PROFIT) {
+    return;
+  }
+  Log.info("position closed due to the target pnl reached", {
+    symbol,
+    data,
+  });
+  await commitClosePending(symbol, {
+    id: "unknown",
+    note: str.newline(
+      "# Позиция закрыта по target pnl",
+    ),
+  });
+});
+
+listenError((error) => {
+  console.log(error);
+  Log.debug("error", {
+    error: errorData(error),
+    message: getErrorMessage(error),
+  });
+});
---
+++ example/content/dec_2025.strategy/dec_2026.test.ts
@@ -0,0 +1,61 @@
+import {
+  addStrategySchema,
+  listenError,
+  Log,
+  Interval,
+  listenIdlePing,
+} from "backtest-kit";
+import { run, extract, File } from "@backtest-kit/pinets";
+import { errorData, getErrorMessage } from "functools-kit";
+
+const PINE_FILE = File.fromPath("btc_dec2025_range.pine", "./math");
+
+const PINE_SCHEMA = {
+  bbUpper: "BB Upper",
+  bbLower: "BB Lower",
+  bbBasis: "BB Basis",
+  rangeHigh: "Range High",
+  rangeLow: "Range Low",
+  signalLine: "Signal Line",
+  close: "Close",
+  signal: "Signal",
+  stopLoss: "StopLoss",
+  takeProfit: "TakeProfit",
+  isRanging: "IsRanging",
+  volSpike: "VolSpike",
+  estimatedTime: "EstimatedTime",
+};
+
+const getPlot = Interval.fn(
+  async (symbol: string) => {
+    const plots = await run(PINE_FILE, { symbol, timeframe: "15m", limit: 100 });
+    return await extract(plots, PINE_SCHEMA) 
+  }, 
+  {
+    interval: "1h",
+  }
+);
+
+addStrategySchema({
+  strategyName: "dec_2025_strategy",
+  getSignal: async (_symbol, when) => {
+    console.log(when);
+    return null;
+  },
+});
+
+listenIdlePing(async ({ symbol }) => {
+  const dump = await getPlot(symbol);
+  Log.info("position dump", {
+    symbol,
+    dump,
+  });
+});
+
+listenError((error) => {
+  console.log(error);
+  Log.debug("error", {
+    error: errorData(error),
+    message: getErrorMessage(error),
+  });
+});
