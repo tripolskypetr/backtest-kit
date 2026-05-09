9a18018313cee212a27cf71a52507a48066626c7
Petr Tripolsky <tripolskypetr@gmail.com>
2026-05-09 20:21:10 +0500

    patch

--- cli/src/helpers/getEntry.ts
+++ cli/src/helpers/getEntry.ts
@@ -1,9 +1,11 @@
 import { fileURLToPath } from 'url';
+import { realpathSync } from 'fs';
 import path from 'path';
 
 export const getEntry = (metaUrl) => {
     const metaPath = fileURLToPath(metaUrl);
-    return path.resolve(process.argv[1]) === path.resolve(metaPath);
+    const realArgv = realpathSync(process.argv[1]);
+    return path.resolve(realArgv) === path.resolve(metaPath);
 };
 
 export default getEntry;
