9ae38f695e004f140b4425c1dc52dcb7c8d4b445
Petr Tripolsky <tripolskypetr@gmail.com>
2026-05-19 21:53:46 +0500

    import-alias

--- cli/src/helpers/overrideModule.ts
+++ cli/src/helpers/overrideModule.ts
@@ -1,21 +1,49 @@
-import Module, { createRequire } from 'module';
+import Module from 'module';
 
-const require = createRequire(import.meta.url);
+type ModuleInstance = Module & {
+  loaded: boolean;
+  filename: string;
+  paths: string[];
+  exports: unknown;
+};
 
 interface ModuleConstructor {
-  _cache: Record<string, NodeModule>;
-  new(id: string): NodeModule;
+  _cache: Record<string, ModuleInstance>;
+  _resolveFilename: (
+    request: string,
+    parent: ModuleInstance | null,
+    isMain: boolean,
+    options?: unknown,
+  ) => string;
+  new(id: string): ModuleInstance;
 }
 
 const ModuleWithCache = Module as unknown as ModuleConstructor;
 
+const VIRTUAL_MAP = new Map<string, string>();
+
+{
+  const originalResolveFilename = ModuleWithCache._resolveFilename;
+  ModuleWithCache._resolveFilename = function (request, parent, isMain, options) {
+    const virtualKey = VIRTUAL_MAP.get(request);
+    if (virtualKey) {
+      return virtualKey;
+    }
+    return originalResolveFilename.call(this, request, parent, isMain, options);
+  };
+}
+
 function overrideModule(moduleName: string, newExports: unknown) {
   const cache = ModuleWithCache._cache;
-  const key = require.resolve(moduleName);
+  const key = moduleName;
+
+  VIRTUAL_MAP.set(moduleName, key);
 
   if (!cache[key]) {
     cache[key] = new ModuleWithCache(key);
     cache[key].loaded = true;
+    cache[key].filename = key;
+    cache[key].paths = [];
   }
 
   cache[key].exports = newExports;
